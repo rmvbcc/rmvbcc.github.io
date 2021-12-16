12fba70cadd9b06e991f64e43073235eb2166a3d
rmvbcc <rmvbcc@126.com>
2021-12-16 14:32:13 +0800

    Create tj.js

---
+++ js/tj.js
@@ -0,0 +1,8 @@
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?ca807efea5edda7088a00994832b7173";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
